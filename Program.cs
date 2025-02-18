33f3654de62242602163249606db103cad55b85d
Jay Wendell Saac <jwsaac@gmail.com>
2025-02-18 13:53:32 +0800

    initial project setup

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿if(args.Length > 0 && File.Exists(args[0]))
+{
+
+}
+else 
+{
+    Console.WriteLine("Enter commands ('EXIT' to quit)");
+    while(true)
+    {
+        var input = Console.ReadLine();
+        input = input?.Trim();
+
+        if(input?.ToUpper() == "EXIT") break;
+    }
+}
